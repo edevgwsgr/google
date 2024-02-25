740d785fddc7cdf15e2a3309486257639fe0e2a1
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-02-25 16:40:14 +0100

    Update obb.js

--- plugins/obb.js
+++ plugins/obb.js
@@ -1,28 +1,75 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' minecraft';
-    let res = await apk(text, conn);
-    await m.reply('_In progress, please wait..._');
-    conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m });
+  if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' minecraft';
+
+  let info = await apkinfo(text);
+  let apkRes = await apk(text, conn);
+
+  await conn.sendMessage(m.chat, {
+    image: { url: info.icon },
+    caption: *Name:* ${info.name}\n*Package:* ${info.packageN},
+    footer: 'Apk files...',
+  });
+
+  await m.reply(_Downloading APK (${info.name}), please wait..._);
+  await conn.sendMessage(
+    m.chat,
+    { document: { url: apkRes.download }, mimetype: apkRes.mimetype, fileName: apkRes.fileName },
+    { quoted: m }
+  );
+
+  if (info.obb) {
+    await m.reply('Sending OBB, please wait...');
+    await conn.sendMessage(m.chat, { document: { url: info.obb_link }, mimetype: 'application/octet-stream', fileName: ${info.packageN}.obb }, { quoted: m });
+  }
 };
-handler.command = /^(obb)$/i;
+
+handler.command = /^(apk2)$/i;
+handler.help = ['apk2'];
+handler.tags = ['downloader'];
+handler.premium = false;
+
 export default handler;
 
-async function apk(url, conn) {
-    let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + encodeURIComponent(url) + '&limit=1');
-    let $ = await res.json();
-    let download = $.datalist.list[0].obb.main.path;
-    let fileName = download.replace(/https:\/\/pool.obb.aptoide.com\//, ' ').match(/(\w*)\/(.*)/)[2].replace(/-/ig, '.');
-    if (!download) throw 'Can\'t download the apk!';
+async function apkinfo(url) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + url + '&limit=1');
+  let $ = await res.json();
 
-    // Check file size before downloading
-    let fileSize = parseInt((await fetch(download, { method: 'head' })).headers.get('content-length'));
-    if (fileSize > 1024 * 1024 * 1024) { // 80 MB
-        throw 'File size exceeds the limit (1024 MB).';
-    }
-
+  try {
     let icon = $.datalist.list[0].icon;
-    let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
-    return { fileName, mimetype, download };
+  } catch {
+    throw 'Can\'t download the apk!';
+  }
+
+  let icon = $.datalist.list[0].icon;
+  let name = $.datalist.list[0].name;
+  let packageN = $.datalist.list[0].package;
+  let download = $.datalist.list[0].file.path;
+  let obb_link;
+  let obb;
+
+  try {
+    obb_link = await $.datalist.list[0].obb.main.path;
+    obb = true;
+  } catch {
+    obb_link = 'not available';
+    obb = false;
+  }
+
+  if (!download) throw 'Can\'t download the apk!';
+  return { obb, obb_link, name, icon, packageN };
+}
+
+async function apk(url, conn) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + encodeURIComponent(url) + '&limit=1');
+  let $ = await res.json();
+  let fileName = $.datalist.list[0].package + '.apk';
+  let download = $.datalist.list[0].file.path;
+  let size = (await fetch(download, { method: 'head' })).headers.get('Content-Length');
+  if (!download) throw 'Can\'t download the apk!';
+  let icon = $.datalist.list[0].icon;
+  let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
+
+  return { fileName, mimetype, download, size };
 }
