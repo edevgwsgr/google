fe9a0c29ccbca7ae0358754828d621ff6b46de50
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-02-25 16:58:39 +0100

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,4 +1,4 @@
-import { createHash } from 'crypto'
+*/import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
 await m.reply(lenguajeGB.smsIDserie())
