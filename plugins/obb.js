1ca393322c26f702310cfb49ae5db2c1a3723955
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-02-26 21:39:44 +0100

    Update obb.js

--- plugins/obb.js
+++ plugins/obb.js
@@ -7,7 +7,6 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
     conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m });
 };
 handler.command = /^(obb)$/i;
-handler.premium = true
 export default handler;
 
 async function apk(url, conn) {
