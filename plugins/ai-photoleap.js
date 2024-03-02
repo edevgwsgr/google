f1284f5dff7f586fd269e3d85036ac27f3d54105
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-03-02 17:09:25 +0100

    Update ai-photoleap.js

--- plugins/ai-photoleap.js
+++ plugins/ai-photoleap.js
@@ -1,43 +1,45 @@
+import axios from "axios"
+
 let handler = async (m, {
     conn,
     args,
     usedPrefix,
     command
 }) => {
-    let text;
+    let text
     if (args.length >= 1) {
-        text = args.slice(0).join(" ");
+        text = args.slice(0).join(" ")
     } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text;
-    } else {
-        throw "Input Teks";
-    }
-    await m.reply(wait);
+        text = m.quoted.text
+    } else throw "Input Teks"
+    await m.reply(wait)
 
     try {
-        let data = await textToImage(text);
+        let data = await textToImage(text)
         if (data) {
+
             await conn.sendFile(m.chat, data.result_url, '', `Image for ${text}`, m, false, {
                 mentions: [m.sender]
             });
+
         }
-    } catch (error) {  // Fix the variable name here
-        await m.reply(error);  // Fix the variable name here
+    } catch (e) {
+        await m.reply(eror)
     }
-};
-
-handler.help = ["photoleap"];
+}
+handler.help = ["photoleap"]
 handler.tags = ["ai"];
-handler.command = /^(imagine)$/i;
+handler.command = /^(imagine2)$/i
+handler.premium = true
 
-export default handler;
+export default handler
 
 /* New Line */
 async function textToImage(text) {
     try {
         const {
             data
-        } = await axios.get("https://tti.photoleapapp.com/api/v1/generate?prompt=" + text);
+        } = await axios.get("https://tti.photoleapapp.com/api/v1/generate?prompt=" + text)
         return data;
     } catch (err) {
         return null;
