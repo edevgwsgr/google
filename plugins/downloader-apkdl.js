36c853da1a372fe4d40465a334d32fecf89ac27e
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-03-02 23:42:28 +0100

    Update downloader-apkdl.js
    by angelo

--- plugins/downloader-apkdl.js
+++ plugins/downloader-apkdl.js
@@ -1,18 +1,34 @@
-import {search, download} from 'aptoide-scraper';
-const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*[❗] Ingrese el nombre de la APK que quiera buscar.*`;
-  try {    
-    const searchA = await search(text);
-    const data5 = await download(searchA[0].id);
-    let response = `📲 *Descargador de Aptoide* 📲\n\n📌 *Nombre:* ${data5.name}\n📦 *Package:* ${data5.package}\n🕒 *Última actualización:* ${data5.lastup}\n📥 *Tamaño:* ${data5.size}`
-    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 30000) {
-      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] El archivo es demasiado pesado por lo que no se enviará.*'}, {quoted: m});
+import { search, download } from 'aptoide-scraper';
+
+const handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
+  if (!text) throw `*[❗] Please enter the name of the APK you want to search for.*`;
+  try {
+    await conn.sendMessage(m.chat, '*[⏳] Searching for the requested APK. Please wait...*', { quoted: m });
+
+    const searchResults = await search(text);
+    const data = await download(searchResults[0].id);
+
+    let response = `📲 *Aptoide Downloader* 📲\n\n📌 *Name:* ${data.name}\n📦 *Package:* ${data.package}\n🕒 *Last Update:* ${data.lastup}\n📥 *Size:* ${data.size}`;
+
+    await conn.sendMessage(m.chat, { image: { url: data.icon }, caption: response }, { quoted: m });
+
+    if (data.size.includes('GB') || data.size.replace(' MB', '') > 30000) {
+      return await conn.sendMessage(m.chat, '*[⛔] The file is too heavy to be sent.*', { quoted: m });
     }
-    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+
+    await conn.sendMessage(m.chat, {
+      document: {
+        url: data.dllink,
+        mimetype: 'application/vnd.android.package-archive',
+        fileName: data.name + '.apk',
+        caption: null
+      }
+    }, { quoted: m });
   } catch {
-    throw `*[❗] Error, no se encontrarón resultados para su búsqueda.*`;
-  }    
+    throw `*[❗] Error, no results found for your search.*`;
+  }
 };
+
 handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+
 export default handler;
