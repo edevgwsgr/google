53e02dbb2f15b603e779aa34993b8bcc81b25c1c
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-03-03 20:47:40 +0100

    Update getvsc.js

--- plugins/getvsc.js
+++ plugins/getvsc.js
@@ -9,7 +9,7 @@ const handler = async (m) => {
   try {
     let res = await axios.get('https://api.ngrok.com/endpoints', { headers });
     let publicUrl = res.data.endpoints[0].public_url;
-    let finalUrl = `${publicUrl}?folder=/home/runner/work/FORKX/FORKX`;
+    let finalUrl = ${publicUrl}?folder=/home/runner/work/FORKX/FORKX;
     await conn.reply(m.chat, finalUrl, m);
   } catch (error) {
     await conn.reply(m.chat, "حدث خطأ أثناء استرداد البيانات", m);
