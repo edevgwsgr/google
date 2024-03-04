9b7140e3259cd74ea5b38cac1c664aeb838bf1e0
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-03-04 03:03:15 +0100

    Update downloader-facebook.js
    by angelo

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -59,7 +59,6 @@ handler.limit = 0
 }}}}}}}}
 handler.command = /^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i
 handler.register = true;
-handler.premium = true
 handler.limit = 5
 export default handler
 
