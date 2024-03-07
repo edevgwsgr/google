ffd0be75d673e87a35ca972ca7f1383a9ee80b64
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-03-07 14:10:09 +0100

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -4,7 +4,8 @@ import { mediafiredl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args }) => {
 	if (!args[0]) throw 'Input URL' 
 	if (!/https?:\/\/(www\.)?mediafire\.com/.test(args[0])) throw 'Invalid URL' 
-	let res = await mediafiredl(args[0])
+	let url = args[0].replace(/^https?:\/\//i, '') // Remove https:// or http:// from the beginning
+	let res = await mediafiredl("https://" + url) // Prepend https:// to the modified URL
 	let mimetype = await lookup(res.url)
 	delete res.url2
 	m.reply(Object.keys(res).map(v => `*• ${v.capitalize()}:* ${res[v]}`).join('\n') + '\n\n_Sending file..._')
@@ -15,3 +16,4 @@ handler.tags = ['downloader']
 handler.command = /^(mediafire)$/i
 //handler.register = true
 export default handler
+
