1a96cb3481a8d1b40a1909774f5ad0601c76a7c1
ANG <106909159+ANGILO856122@users.noreply.github.com>
2024-03-07 14:24:33 +0100

    Update downloader-mediafire.js
    by angelo

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -2,23 +2,17 @@ import { lookup } from 'mime-types'
 import { mediafiredl } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args }) => {
-    if (!args[0]) throw 'Input URL' 
-    if (!/https?:\/\/(www\.)?mediafire\.com/.test(args[0])) throw 'Invalid URL' 
-    let url = args[0].replace(/^https?:\/\//i, '') // Remove https:// or http:// from the beginning
-    let res = await mediafiredl("https://" + url) // Prepend https:// to the modified URL
-    let mimetype = await lookup(res.url)
-    delete res.url2
-    
-    let message = `• Filename: ${res.filename}\n`
-    message += '_Sending file..._'
-
-    m.reply(message)
-    conn.sendMessage(m.chat, { document: { url: res.url }, fileName: res.filename, mimetype }, { quoted: m })
+	if (!args[0]) throw 'Input URL' 
+	if (!/https?:\/\/(www\.)?mediafire\.com/.test(args[0])) throw 'Invalid URL' 
+	let res = await mediafiredl(args[0])
+	let mimetype = await lookup(res.url)
+	delete res.url
+	delete res.url2
+	m.reply(Object.keys(res).map(v => *• ${v.capitalize()} :* ${res[v]}).join('\n') + '\n\n_Sending file..._')
+	conn.sendMessage(m.chat, { document : { url : res.url2 }, fileName : res.filename, mimetype }, { quoted: m })
 }
 handler.help = handler.alias = ['mediafire']
 handler.tags = ['downloader']
 handler.command = /^(mediafire)$/i
 //handler.register = true
 export default handler
-
-
