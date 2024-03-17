dc579840043efaf578f1c954dc84ae6a508a05f2
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-03-17 14:36:01 +0000

    Update ai-play.js

--- plugins/ai-play.js
+++ plugins/ai-play.js
@@ -1,75 +1,127 @@
-import fetch from "node-fetch";
-import yts from "yt-search";
-import ytdl from 'ytdl-core';
-import axios from 'axios';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+const {
+	inrl,
+	extractUrlsFromString,
+	searchYT,
+	getYTInfo,
+	GenListMessage,
+	AudioMetaData,
+	downloadMp3,
+	downloadMp4,
+	getBuffer,
+	toAudio,
+	lang,
+	mode
+} = require('../lib');
 
-let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-    let q, v, yt, dl_url, ttl, size, lolhuman, lolh, n, n2, n3, n4, cap, qu, currentQuality;
-    
-    // Check if text is empty
-    if (!text) {
-        return conn.reply(m.chat, `يرجى استخدام الأمر بالتالي: ${usedPrefix}${command} [اسم الأغنية]`, m)
-    }
-    
-    try {
-        const yt_play = await search(text);
-        let additionalText = '';
-        if (command === 'play') {
-            additionalText = '🔊 المشغل الآلي';
-        } else if (command === 'rffewfw') {
-            additionalText = '🎥 الفيديو الموسيقي';
-        }
-        let searchMessage = `═════ •⊰JEEN⊱• ═════\n`;
-        searchMessage += `🔖 ${text}\n`;
-        searchMessage += `🗣 ${yt_play[0].author.name}\n`;
-        searchMessage += `🔊 ${additionalText}\n`;
-        searchMessage += `═════ •⊰JEEN⊱• ═════\n`;
-        
-        await conn.sendMessage(m.chat, {
-            text: searchMessage,
-            contextInfo: {
-                externalAdReply: {
-                    title: yt_play[0].title,
-                    body: packname,
-                    thumbnailUrl: yt_play[0].thumbnail, 
-                    mediaType: 1,
-                    showAdAttribution: true,
-                    renderLargerThumbnail: true
-                }
-            }
-        }, { quoted: m });
-        
-        if (command == 'play') {
-            try {
-                let q = '128kbps';
-                let v = yt_play[0].url;
-                const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v));
-                const dl_url = await yt.audio[q].download();
-                const ttl = await yt.title;
-                const size = await yt.audio[q].fileSizeH;
-                
-                // Send the audio without a thumbnail
-                await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/mpeg' }, { quoted: m });
-                conn.sendMessage(m.chat, 'done', 'conversation', { quoted: m });
-            } catch {
-                // Handle error
-            }
-        }
-        // Add more conditions for other commands like 'rffewfw' if needed
-    } catch {
-        // Handle error
-    }
-}
-
-handler.command = ['play', 'rffewfw'];
-handler.register = true;
-handler.exp = 0;
-handler.limit = 4;
-
-export default handler;
-
-async function search(query, options = {}) {
-    const search = await yts.search({ query, hl: "ar", gl: "ES", ...options });
-    return search.videos;
-}
+inrl({
+	pattern: 'play',
+	type: "downloader",
+	desc: lang.YT.PLAY_DESC,
+	fromMe: mode
+}, async (message, match) => {
+	match = match || message.reply_message.text;
+		if (!match) return await message.send(lang.YT.NEED_TEXT);
+		const url = await extractUrlsFromString(match);
+		if (!url[0]) {
+			const result = await searchYT(match);
+			if (!result[0]) return await message.send(lang.BASE.ERROR.format('_Not Found_'));
+			const {
+				title,
+				publishDate,
+				viewCount,
+				thumbnail
+			} = await getYTInfo(result[0].url);
+			return await message.sendReply(thumbnail, {
+				caption: GenListMessage(title, ["• video", "• video document", "• audio", "• audio document"], false, "\n_Send number as reply to download_")
+			}, "image");
+		} else {
+			const {
+				title,
+				publishDate,
+				viewCount,
+				thumbnail
+			} = await getYTInfo(url[0]);
+			return await message.sendReply(thumbnail, {
+				caption: GenListMessage(title, ["• video", "• video document", "• audio", "• audio document"], false, "\n_Send number as reply to download_")
+			}, "image");
+		}
+});
+inrl({
+	on: 'text',
+	fromMe: mode
+}, async (message, match) => {
+	if (!message.reply_message?.fromMe || !message.reply_message?.text) return;
+	if (!message.reply_message.text.includes('_Send number as reply to download_')) return;
+		if (message.body.includes("• audio document")) {
+			match = message.body.replace("• audio document", "").trim();
+			await message.send(lang.BASE.DOWNLOAD.format(match));
+			const result = await searchYT(match.replace('•', ''));
+			const {
+				seconds,
+				title,
+				thumbnail
+			} = await getYTInfo(result[0].url);
+		        const ress = await downloadMp3(result[0].url);
+			const AudioMeta = await AudioMetaData(await toAudio(ress),{title,image:thumbnail});
+			return await message.client.sendMessage(message.from, {
+				document: AudioMeta,
+				mimetype: 'audio/mpeg',
+				fileName: title.replaceAll(' ', '-') + ".mp3"
+			}, {
+				quoted: message.data
+			});
+		} else if (message.body.includes("• audio")) {
+			match = message.body.replace("• audio", "").trim();
+			await message.send(lang.BASE.DOWNLOAD.format(match));
+			const result = await searchYT(match.replace('•', ''));
+			const {
+				seconds,
+				title,
+				thumbnail
+			} = await getYTInfo(result[0].url);
+		        const ress = await downloadMp3(result[0].url);
+			const AudioMeta = await AudioMetaData(await toAudio(ress),{title,image:thumbnail});
+			return await message.client.sendMessage(message.jid, {
+				audio: AudioMeta,
+				mimetype: 'audio/mpeg',
+				fileName: title.replaceAll(' ', '-') + ".mp3"
+			}, {
+				quoted: message.data
+			});
+		} else if (message.body.includes("• video document")) {
+			match = message.body.replace("• video document", "").trim();
+			await message.send(lang.BASE.DOWNLOAD.format(match));
+			const result = await searchYT(match.replace('•', ''));
+			const {
+				seconds,
+				title,
+				thumbnail
+			} = await getYTInfo(result[0].url);
+		        const ress = await downloadMp4(result[0].url);
+			return await message.client.sendMessage(message.from, {
+				document: ress,
+				mimetype:  'video/mp4',
+				fileName: title.replaceAll(' ', '-') + ".mp4"
+			}, {
+				quoted: message.data
+			});
+		} else if (message.body.includes("• video")) {
+			match = message.body.replace("• video", "").trim();
+			await message.send(`*_downloading_*\n*_${match}_*`);
+			const result = await searchYT(match.replace('•', ''));
+			const {
+				seconds,
+				title,
+				thumbnail
+			} = await getYTInfo(result[0].url);
+		        const ress = await downloadMp4(result[0].url);
+			return await message.client.sendMessage(message.from, {
+				video: ress,
+				mimetype: 'video/mp4',
+				fileName: title.replaceAll(' ', '-') + ".mp4",
+				caption: title
+			}, {
+				quoted: message.data
+			});
+		}
+});
