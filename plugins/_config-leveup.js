d3a841d147e5b8fcc8ed38171bd9ab4dcfb9a813
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-03-17 03:39:34 +0000

    Update _config-leveup.js

--- plugins/_config-leveup.js
+++ plugins/_config-leveup.js
@@ -1,63 +1,119 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-let handler = async (m, { conn }) => {
-	  let name = conn.getName(m.sender)
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg')
-    let user = global.db.data.users[m.sender]
-    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-        let { min, xp, max } = xpRange(user.level, global.multiplier)
-        let txt = `
-┌───⊷ *NIVEL*
-▢ Nombre : *${name}*
-▢ Nivel : *${user.level}*
-▢ XP : *${user.exp - min}/${xp}*
-▢ Rango : *${user.role}*
-└──────────────
+const levelupHandler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dirname, isOwner, text, isAdmin, isROwner }) => {
+  const { xpRange } = require('../lib/levelling.js')
+  const { levelup } = require('../lib/canvas.js')
+  let { exp, limit, level, role } = global.db.data.users[m.sender]
+  let { min, xp, max } = xpRange(level, global.multiplier)
 
-Te falta *${max - user.exp}* de *XP* para subir de nivel
+  let d = new Date(new Date + 3600000)
+  let locale = 'es'
+  let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+  let week = d.toLocaleDateString(locale, { weekday: 'long' })
+  let date = d.toLocaleDateString(locale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric' 
+  })
+  let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }).format(d)
+  let time = d.toLocaleTimeString(locale, {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  })
+  let _uptime = process.uptime() * 1000
+  let _muptime
+  if (process.send) {
+    process.send('uptime')
+    _muptime = await new Promise(resolve => {
+      process.once('message', resolve)
+      setTimeout(resolve, 1000)
+    }) * 1000
+  }
+  let { money } = global.db.data.users[m.sender]
+  let muptime = clockString(_muptime)
+  let uptime = clockString(_uptime)
+  let totalreg = Object.keys(global.db.data.users).length
+  let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+  let replace = {
+    '%': '%',
+    p: _p, uptime, muptime,
+    me: conn.getName(conn.user.jid),
+
+    exp: exp - min,
+    maxexp: xp,
+    totalexp: exp,
+    xp4levelup: max - exp,
+
+    level, limit, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+    readmore: readMore
+  }
+  text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+  
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let mentionedJid = [who]
+  let username = conn.getName(who)
+
+  let name = conn.getName(m.sender)
+  let user = global.db.data.users[m.sender]
+  if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+    let { min, xp, max } = xpRange(user.level, global.multiplier)
+    throw `
+╭─ ❖ ── *Level* ── ❖ ──╗
+┃ *Nombre :* ${name}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *Nivel:* *${user.level}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *Rango:* ${user.role}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *XP:* *${user.exp - min}/${xp}*
+╰─ ❖ ── ✦ ── ✦ ── ❖ ──╝
+
+*Te falta ${max - user.exp} de XP para subir de nivel*
 `.trim()
-try {
-  let imgg = API('fgmods', '/api/maker/rank', {
-    username: name,
-    xp: user.exp - min,
-    exp: xp,
-    avatar: pp,
-    level: user.level,
-    ranklog: 'https://i.ibb.co/7gfnyMw/gold.png',
-    background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
-}, 'apikey')
+  }
+  
+  let before = user.level * 1
+  while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+  if (before !== user.level) {
+    let teks = `Bien hecho! ${conn.getName(m.sender)} Nivel: ${user.level}`
+    let str = `
+╭─ ❖ ── *LevelUp* ── ❖ ──╗
+┃ *NIVEL ANTERIOR:* *${before}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *NIVEL ACTUAL:* *${user.level}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *RANGO* ${user.role}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *FECHA:* *${new Date().toLocaleString('id-ID')}*
+╰─ ❖ ── ✦ ── ✦ ── ❖ ──╝
 
-    conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
-} catch (e) {
-    m.reply(txt)
-}
-    }
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-    if (before !== user.level) {
-    	user.role = global.rpg.role(user.level).name
-
-        let str = `
-┌─⊷ *LEVEL UP*
-▢ Nivel anterior : *${before}*
-▢ Nivel actual : *${user.level}*
-▢ Rango : *${user.role}*
-└──────────────
-
-*_Cuanto más interactúes con los bots, mayor será tu nivel_*
+*_Cuanto más interactúes con KatashiBot-MD, mayor será tu nivel!!_*
+*_Actualiza tú rango con el comando ${usedPrefix}rol!!_*
 `.trim()
-        try {
-            let img = API('fgmods', '/api/maker/levelup', { 
-                avatar: pp 
-             }, 'apikey')
-      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
-        } catch (e) {
-            m.reply(str)
-        }
+    try {
+      const img = await levelup(teks, user.level)
+      conn.sendMessage(m.chat, {image: {url: img}, caption: str, mentions: conn.parseMention(str)}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+    } catch (e) {
+      m.reply(str)
     }
+  }
 }
 
-handler.help = ['levelup']
-handler.tags = ['econ']
-handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
+levelupHandler.help = ['levelup']
+levelupHandler.tags = ['xp']
+levelupHandler.command = ['nivel', 'lvl', 'levelup', 'level']
+levelupHandler.exp = 0
 
-export default handler
+module.exports = levelupHandler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
