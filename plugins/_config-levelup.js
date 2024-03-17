c3d8f86930a257c2b1da041f3bd57257ad4eb65b
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-03-17 15:27:16 +0000

    Update _config-levelup.js

--- plugins/_config-levelup.js
+++ plugins/_config-levelup.js
@@ -1,59 +1,54 @@
-import fetch from 'node-fetch'
 import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+import can from 'knights-canvas'
 
-let handler = async (m, { conn, usedPrefix }) => {
-  // let pp = './src/avatar_contact.png'
-  let who = m.sender
-  let name = conn.getName(m.sender)
-  let discriminator = who.substring(9, 13)
-  try {
-    pp = await conn.getProfilePicture(who)
-  } catch (e) {
-  } finally {
-    let user = global.db.data.users[m.sender]
-    let users = Object.entries(global.db.data.users).map(([key, value]) => {
-      return { ...value, jid: key }
-    })
-    let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
-    let usersLevel = sortedLevel.map(enumGetKey)
-    let { min, xp, max } = xpRange(user.level, global.multiplier)
-    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-      let rank = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=birdy-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=DIKIT%20LAGI%20NAIK'
-        {
-          await conn.sendButtonImg(m.chat, await (await fetch(rank)).buffer(), `Level ${name} ${user.level} (${user.exp - min}/${xp})\nKurang ${max - user.exp} EXP lagi!`.trim(), wm, 'Enable autolevelup', `${usedPrefix}on autolevelup`, m)
-        }
-    }
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-    if (before !== user.level) {
-      let rank = 'https://telegra.ph/file/5acbd203b92c03cdde356.jpg'
-        {
-          await conn.sendButtonImg(m.chat, await (await fetch(rank)).buffer(), `${name} Level Up!\n_${before}_ -> ${user.level}`.trim(), wm, 'AUTO LEVEL UP', `${usedPrefix}on autolevelup`)
-          //await conn.sendButtonLoc(m.chat, await (await fetch(rank)).buffer(), `${name} Level Up!\n_${before}_ -> ${user.level}`.trim(), wm, 'AUTO LEVEL UP', `${usedPrefix}on autolevelup`, m)
-        }
-    }
-  }
+let handler = async (m, { conn }) => {
+
+function test(num, size) {
+var s = num+''
+while (s.length < size) s = '0' + s
+return s
 }
 
+let user = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let whoPP = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let ppBot = await conn.profilePictureUrl(whoPP, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+let image = await new can.Rank().setAvatar(ppBot).setUsername(name ? name.replaceAll('\n','') : '-').setBg('https://telegra.ph/file/fde739f66f1b81a43fe54.jpg').setNeedxp(wm).setCurrxp(`${user.exp}`).setLevel(`${user.level}`).setRank('https://i.ibb.co/Wn9cvnv/FABLED.png').toAttachment()
+let data = image.toBuffer()
+
+let { role } = global.db.data.users[m.sender]
+if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+
+let le = `*Name* ${name}
+
+Level : *${user.level}* 📊
+XP : *${user.exp - min} / ${xp}*
+
+Not enough XP *${max - user.exp}* Again! ✨`
+await conn.sendMessage(m.chat, { image: data, caption: le }, { quoted: m })
+}
+let before = user.level * 1
+while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+if (before !== user.level) {
+
+let str = `*🥳 New level 🥳* 
+*• 🧬 Previous level :* ${before}
+*• 🧬 New levels :* ${user.level}
+*• 📅 Date :* ${new Date().toLocaleString('id-ID')}
+
+*Note:* _Chont more often interact with the bot, the greater your level_`
+try {
+await conn.sendMessage(m.chat, { image: data, caption: str }, { quoted: m })
+} catch (e) {
+m.reply(str)
+}}
+
+}
 handler.help = ['levelup']
-handler.tags = ['xp']
-
-handler.command = /^levelup$/i
-
-export default handler
-
-function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-  else return (...args) => args[ascending & 1] - args[!ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return { ...b[i], [property]: a[property] === undefined ? _default : a[property] }
-  }
-  else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-  return a.jid
-}
+handler.tags = ['rg']
+handler.command = ['nivel']
+handler.register = true
+export default handler;
