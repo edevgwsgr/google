5a002b94c3ebc1ae1380ee336ec9dcc29268a59a
GitHub <action@github.com>
2024-03-26 17:35:42 +0000

    out session update

---
+++ plugins/deidara.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+const handler = async (m, usedPrefix, command) => {
+  const g = await fetch(`https://raw.githubusercontent.com/inirey/RESTAPI/master/data/deidara.json`);
+  const f = await g.json();
+  const a = f[Math.floor(Math.random() * f.length)];
+  conn.sendFile(m.chat, a, "deidara.jpg", 'instagram.com/majnon._.98', m);
+};
+
+handler.help = ["deidara"];
+handler.tags = ["anime"];
+handler.command = /^anim$/i;
+
+export default handler;
