e9c18555e95dbdc7880b6df5f371c2b139c5ec6f
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-03-26 00:54:20 +0000

    Update ai-imganime.js

--- plugins/ai-imganime.js
+++ plugins/ai-imganime.js
@@ -1,27 +1,29 @@
 import uploadImage from '../lib/uploadImage.js';
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-const q = m.quoted ? m.quoted : m;
-const mime = (q.msg || q).mimetype || q.mediaType || '';
-if (!/image/g.test(mime)) throw `${lenguajeGB.smsAvisoMG()}𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝙊 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉`
-  m.reply(`${lenguajeGB.smsAvisoIIG()}𝘼𝙂𝙐𝘼𝙍𝘿𝙀 𝙀𝙎𝙏𝙊𝙔 𝘾𝙊𝙉𝙑𝙄𝙍𝙏𝙄𝙀𝙉𝘿𝙊 𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝘼 𝘿𝙄𝙎𝙀𝙉̃𝙊 𝘼𝙉𝙄𝙈𝙀, 𝙎𝙀𝘼 𝙋𝘼𝘾𝙄𝙀𝙉𝙏𝙀 𝙀𝙉 𝙇𝙊 𝙌𝙐𝙀 𝙀𝙉𝙑𝙄𝙊 𝙀𝙇 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊`);
-const data = await q.download?.();
-const image = await uploadImage(data);
-try {
-const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
-await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
-} catch (i) {
-try {
-const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
-await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
-} catch (a) {
-try {
-const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
-await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
-} catch (e) {
-throw `${lenguajeGB.smsAvisoFG()}𝙀𝙍𝙍𝙊𝙍, 𝙑𝙀𝙍𝙄𝙁𝙄𝙌𝙐𝙀 𝙌𝙐𝙀 𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙎𝙀𝘼 𝙀𝙇 𝙍𝙊𝙎𝙏𝙍𝙊 𝘿𝙀 𝙐𝙉𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼`
-}}}}
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || q.mediaType || '';
+  if (!/image/g.test(mime)) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*';
+  m.reply('*[❗] 𝙲𝙾𝙽𝚅𝙸𝙴𝚁𝚃𝙸𝙴𝙽𝙳𝙾 𝙸𝙼𝙰𝙶𝙴𝙽 𝙰 𝙳𝙸𝚂𝙴𝙽̃𝙾 𝙰𝙽𝙸𝙼𝙴, 𝚂𝙴𝙰 𝙿𝙰𝙲𝙸𝙴𝙽𝚃𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝙴𝙻 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾*');
+  const data = await q.download?.();
+  const image = await uploadImage(data);
+  try {
+    const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
+    await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
+  } catch (i) {
+    try {
+      const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
+      await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
+    } catch (a) {
+      try {
+        const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
+        await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
+      } catch (e) {
+        throw '*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝙴𝚁𝙸𝙵𝙸𝚀𝚄𝙴 𝚀𝚄𝙴 𝙴𝙽 𝙻𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚂𝙴𝙰 𝚅𝙸𝚂𝙸𝙱𝙻𝙴 𝙴𝙻 𝚁𝙾𝚂𝚃𝚁𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰*';
+      }
+    }
+  }
+};
 handler.help = ['toanime'];
 handler.tags = ['tools'];
 handler.command = /^(imganime|toanime)$/i;
-handler.limit = 4
 export default handler;
