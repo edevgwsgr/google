d953d267eebfa240ff3c17793f27a8a26c329434
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-03-30 06:21:08 +0000

    Update bing.js

--- plugins/bing.js
+++ plugins/bing.js
@@ -1,19 +1,66 @@
-import fetch from 'node-fetch'
+const fetch = require('node-fetch');
 
-let handler = async (m, { conn, text }) => {
-    if (!text) throw 'What do you want to create?'
-    m.react(rwait)
-    let msg = encodeURIComponent(text)
-    let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
-    let data = await res.json()
-    console.log(data)
-    let buffer = data.result
-    conn.sendFile(m.chat, buffer, 'image.png', `${text}`, m)
-    m.react(done)
+let handler = async (m, {
+  conn,
+  text,
+  usedPrefix,
+  command
+}) => {
+  if (command == 'bing') {
+    if (!text) throw `Example : ${usedPrefix + command} siapa presiden Indonesia?`;
+    try {
+      m.reply(wait)
+      let response = await fetch('https://api.botcahx.eu.org/api/search/bing-chat', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            text: text,
+            apikey: btc
+          })
+        })
+        .then(res => res.json());
+
+      await conn.reply(m.chat, response.message, m);
+    } catch (e) {
+      console.log(e);
+      throw `*Error:* ${eror}`;
+    }
+  }
+  if (command == 'bingimg') {
+    if (!text) throw `Contoh: ${usedPrefix + command} anak berlari menggunakan pakaian merah 3d animation`;
+    try {
+      m.reply(wait)
+      let response = await fetch('https://api.botcahx.eu.org/api/search/bing-img', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            text: text,
+            apikey: btc
+          })
+        })
+        .then(res => res.json());
+
+      for (let i = 0; i < 4; i++) {
+        let img = response.result[i]
+        await sleep(3000)
+        await conn.sendFile(m.chat, img, 'bing_img.png', `*PROMPT:* ${text}`, m)
+      }
+    } catch (error) {
+      throw `${eror}`
+    }
+  }
 }
 
-handler.help = ['bingimg <query>']
-handler.tags = ['AI']
-handler.command = /^bingimg$/i
+handler.command = handler.help = ['bing', 'bingimg']
+handler.tags = ['tools']
+handler.limit = true
 
-export default handler
+module.exports = handler
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
