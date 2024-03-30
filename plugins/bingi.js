4331af19cce448dd62dc2072628e41dc08a1b42f
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-03-30 03:31:16 +0000

    Update bingi.js

--- plugins/bingi.js
+++ plugins/bingi.js
@@ -1,28 +1,32 @@
 import puppeteer from 'puppeteer-core';
 
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'What do you want to create?';
-    m.react('⌛');
-    
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`https://www.bing.com/images/create?q=${encodeURIComponent(text)}`);
-    
-    // Set viewport size
-    await page.setViewport({ width: 1920, height: 1080 });
+    try {
+        if (!text) throw 'Please provide a search query.';
+        m.react('⌛');
+        
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`https://www.bing.com/images/create?q=${encodeURIComponent(text)}`);
+        
+        // Set viewport size
+        await page.setViewport({ width: 1920, height: 1080 });
 
-    // Wait for any animations or loading to complete
-    await page.waitForTimeout(3000);
+        // Wait for any animations or loading to complete
+        await page.waitForTimeout(5000); // Increased waiting time to ensure proper loading
 
-    // Take a screenshot of the page
-    const screenshot = await page.screenshot({ fullPage: true });
+        // Take a screenshot of the page
+        const screenshot = await page.screenshot({ fullPage: true });
 
-    // Close the browser
-    await browser.close();
+        // Close the browser
+        await browser.close();
 
-    // Send image
-    conn.sendFile(m.chat, screenshot, 'image.png', `${text}`, m);
-    m.react('✅');
+        // Send image
+        conn.sendFile(m.chat, screenshot, 'image.png', `${text}`, m);
+        m.react('✅');
+    } catch (error) {
+        m.reply(error);
+    }
 }
 
 handler.help = ['createimg <text>'];
