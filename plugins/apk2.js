53d71afdb15f62fc905d8f4edff0ae8d000c9249
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-04-08 16:00:26 +0000

    Update apk2.js

--- plugins/apk2.js
+++ plugins/apk2.js
@@ -1,34 +1,47 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, text, usedPrefix, command, sender }) => {
-    if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' <APK URL>';
-    let res = await apk(text, sender);
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+  if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' minecraft';
+  let res = await apk(text);
 
-    await conn.sendMessage(m.chat, {
-        text: `Downloading APK...`,
-    });
+  m.react(rwait);
 
-    await conn.sendMessage(
-        m.chat,
-        { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName },
-        { quoted: m }
-    );
+  await conn.sendMessage(m.chat, {
+    text: `Downloading ${res.fileName}...`,
+  });
+
+  await conn.sendMessage(
+    m.chat,
+    { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName },
+    { quoted: m }
+  );
 };
 
 handler.command = /^(apk2)$/i;
-handler.help = ['apk <APK URL>'];
+handler.help = ['apk'];
 handler.tags = ['downloader'];
-handler.premium = false;
+handler.premium = true;
+handler.group = false;
 export default handler;
 
-async function apk(url, sender) {
-    let fileName = url.substring(url.lastIndexOf('/') + 1); // Extract file name from URL
-    let download = url;
-    let size = (await fetch(download, { method: 'head' })).headers.get('Content-Length');
+async function apk(url) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + encodeURIComponent(url) + '&limit=1');
+  let $ = await res.json();
+  
+  let packageN = $.datalist.list[0].package;
+  let download = $.datalist.list[0].file.path;
+  let icon = $.datalist.list[0].icon;
+  let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
+  
+  // Check if the file extension is APK
+  if (!download.endsWith('.apk')) throw 'Can\'t download the apk!';
 
-    if (!size) throw 'Can\'t download the APK!';
-    // Remove the size limit check to allow downloading files larger than 10 GB
-    let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
+  // Get the file size
+  let size = (await fetch(download, { method: 'head' })).headers.get('Content-Length');
 
-    return { fileName, mimetype, download, size };
+  if (!download) throw 'Can\'t download the apk!';
+
+  let fileName = packageN + '.apk';
+
+  return { fileName, mimetype, download, size };
 }
