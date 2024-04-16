6c06c8b202acb3bd7c44d81bdcc777d59007ff4a
GitHub <action@github.com>
2024-04-16 07:14:19 +0000

    out session update

--- plugins/playstore.js
+++ plugins/playstore.js
@@ -1,31 +1,24 @@
-import gplay from "google-play-scraper";
+import fetch from 'node-fetch'
+import translate from '@vitalets/google-translate-api'
+let handler = async (m, { conn, text, args }) => {
+if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
+try {
+let enc = encodeURIComponent(text)
+let json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=brunosobrino&q=${enc}`)
+let gPlay = await json.json()
 
-let handler = async (m, { conn, text }) => {
-  if (!text) throw "*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*";
-  let res = await gplay.search({ term: text });
-  if (!res.length) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`;
-  let opt = {
-    contextInfo: {
-      externalAdReply: {
-        title: res[0].title,
-        body: res[0].summary,
-        thumbnail: (await conn.getFile(res[0].icon)).data,
-        sourceUrl: res[0].url,
-      },
-    },
-  };
-  await console.log(res);
-  res = res.map(
-    (v) =>
-      `*🔍 Resultado:* ${v.title}
-       *✍️ Desarrollador:* ${v.developer}
-       *💸 Precio:* ${v.priceText}
-       *📈 Puntuacion:* ${v.scoreText}
-        *⛓️ Link:* ${v.url}`
-  ).join`\n\n`;
-  m.reply(res, null, opt);
-};
-handler.help = ['playstore <aplicacion>'];
-handler.tags = ['internet'];
-handler.command = /^(playstore)$/i;
-export default handler;
+let mystic = await translate(`${gPlay.descripcion}`, { to: 'es', autoCorrect: true })
+if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`)
+conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 Resultado: ${gPlay.titulo}
+🧬 I: ${gPlay.id}
+⛓️ Link: ${gPlay.link}
+🖼️ I: ${gPlay.imagen}
+✍️ D: ${gPlay.desarrollador}
+📈 P: ${gPlay.puntuacion}`},{quoted:m})
+} catch {
+await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')    
+}}
+handler.help = ['playstore <aplicacion>']
+handler.tags = ['internet']
+handler.command = /^(playstore)$/i
+export default handler
